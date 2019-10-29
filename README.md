de434076d84706130664ffdfc069d7db027fbbae
snk8580 <39210224+snk8580@users.noreply.github.com>
2019-10-29 17:53:13 +0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# testing-object
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+`;
+
+// 6
+const data = [6, 4, 0, 3, -2, 1];
+console.log(data.sort());
+
+// 7.
+const dog = ["dog"];
+console.log(dog.splice(0));
